d2d6a540fead4d830ec03167e8c1daa411ff82c9
qxkya <93773645+qxkya@users.noreply.github.com>
2024-10-30 10:31:20 +0100

    Create Scripts.lua

---
+++ Scripts.lua
@@ -0,0 +1,19 @@
+return {
+    ["The Survival Game"] = {
+        {
+            ["Script_Name"] = "Aura Paid",
+            ["Place_Id"] = 11156779721,
+            ["Status"] = "stable",
+            ["Script_Url"] = "https://kya.jvck.gg/aura"
+        }
+    },
+
+    ["Unnamed Shooter"] = {
+        {
+            ["Script_Name"] = "Aura Paid"
+            ["Place_Id"] = 14202073004,
+            ["Status"] = "stable",
+            ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/UnnamedShooter.lua"
+        }
+    }
+}
