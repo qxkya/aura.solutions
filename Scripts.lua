d8384b27db5c7f27c811d2049bf161c2d104d728
qxkya <93773645+qxkya@users.noreply.github.com>
2025-01-30 14:41:58 +0100

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -3,15 +3,8 @@ return {
         {
             ["Script_Name"] = "Aura Paid",
             ["Place_Id"] = 11156779721,
-            ["Status"] = "stable",
+            ["Status"] = "mildly unstable",
             ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/Games/TheSurvivalGame/main.luau"
-        },
-        
-        {
-            ["Script_Name"] = "Ore Autofarm",
-            ["Place_Id"] = 11156779721,
-            ["Status"] = "unstable",
-            ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/OreFarm_TheSurvivalGame.lua"
         }
     },
 
@@ -19,14 +12,14 @@ return {
         {
             ["Script_Name"] = "Aura Paid",
             ["Place_Id"] = 16732694052,
-            ["Status"] = "beta",
+            ["Status"] = "temporarily disabled",
             ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/Games/Fisch/main.luau"
         },
 
         {
             ["Script_Name"] = "Aura Paid Rewrite",
             ["Place_Id"] = 16732694052,
-            ["Status"] = "not complete",
+            ["Status"] = "stable",
             ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/Games/Fisch/main_rewrite.luau"
         }
     }
