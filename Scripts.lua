2d17c4b561bb88555649d02523cdc97c4e1f20a0
qxkya <93773645+qxkya@users.noreply.github.com>
2025-02-11 16:42:08 +0100

    Update Scripts.lua

--- Scripts.lua
+++ Scripts.lua
@@ -12,15 +12,8 @@ return {
         {
             ["Script_Name"] = "Aura Paid",
             ["Place_Id"] = 16732694052,
-            ["Status"] = "temporarily disabled",
+            ["Status"] = "updated",
             ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/Games/Fisch/main.luau"
-        },
-
-        {
-            ["Script_Name"] = "Aura Paid Rewrite",
-            ["Place_Id"] = 16732694052,
-            ["Status"] = "stable",
-            ["Script_Url"] = "https://raw.githubusercontent.com/qxkya/aura.solutions/refs/heads/main/Games/Fisch/main_rewrite.luau"
         }
     }
 }
